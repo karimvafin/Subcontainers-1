d4ac952bb5f98f8444208b3b0aa009684b1394e8
Abrackadabrr <iliynea2002@ya.ru>
2021-02-05 18:56:42 +0300

    Добавил файлы из прошлого семестра

---
+++ subforwardlist.cpp
@@ -0,0 +1,143 @@
+﻿#include <iostream>
+using namespace std;
+
+struct subforwardlist
+{
+	int data;
+	subforwardlist* next;
+};
+
+bool init(subforwardlist** sfl)
+{
+	*sfl = NULL;
+	return true;
+}
+
+bool push_back(subforwardlist** sfl, int d)
+{
+	subforwardlist* tmp = new subforwardlist;
+	tmp->data = d;
+	subforwardlist* tmp1 = *sfl;
+	while (*sfl != NULL)
+	{
+		*sfl = (*sfl)->next;
+	}
+	if (*sfl == tmp1) cout << "warning" << endl;
+	*sfl = tmp;
+	(*sfl)->next = NULL;
+	return true;
+}
+
+int pop_back(subforwardlist** sfl)
+{
+	int i = 0;
+	while ((*sfl)->next != NULL)
+	{
+		*sfl = (*sfl)->next;
+		i++;
+	}
+	cout << i << endl;
+	int e = (*sfl)->data;
+	delete *sfl;
+	*sfl = NULL;
+	return e;
+}
+
+bool push_forward(subforwardlist** sfl, int d)
+{
+	subforwardlist* tmp = new subforwardlist;
+	tmp->next = (*sfl);
+	tmp->data = d;
+	*sfl = tmp;
+	return true;
+}
+
+int pop_forward(subforwardlist** sfl)
+{
+	subforwardlist* tmp = (*sfl)->next;
+	int e = (*sfl)->data;
+	delete *sfl;
+	*sfl = tmp;
+	return e;
+}
+
+bool push_where(subforwardlist** sfl, unsigned int where, int d)
+{
+	subforwardlist* tmp = new subforwardlist;
+	tmp->data = d;
+	for (int i = 1; i < where; i++)
+	{
+		(*sfl) = (*sfl)->next;
+	}
+	tmp->next = (*sfl)->next;
+	*sfl = tmp;
+	return true;
+}
+
+bool erase_where(subforwardlist** sfl, unsigned int where)
+{
+	for (int i = 1; i < where; i++)
+	{
+		(*sfl) = (*sfl)->next;
+	}
+	subforwardlist* tmp = (*sfl)->next->next;
+	delete (*sfl)->next;
+	(*sfl)->next = tmp;
+	return true;
+}
+
+unsigned int size(subforwardlist* sfl)
+{
+	unsigned int size = 0;
+	while (sfl != NULL)
+	{
+		sfl = (sfl)->next;
+		size++;
+	}
+	return size;
+}
+
+void clear(subforwardlist** sfl)
+{
+	if (*sfl == NULL) return;
+	clear(&((*sfl)->next));
+	delete *sfl;
+	*sfl = NULL;
+	return;
+}
+
+void print(subforwardlist* sfl)
+{
+	while (sfl != NULL)
+	{
+		cout << sfl->data << " ";
+		sfl = (sfl)->next;
+	}
+	cout << endl;
+	return;
+}
+
+int main()
+{
+	subforwardlist* start;
+	init(&start);
+
+	push_back(&start, 1);
+	cout << size(start) << endl;
+	print(start);
+	cout << endl;
+	push_forward(&start, 5);
+	cout << size(start) << endl;
+	print(start);
+	cout << endl;
+	push_forward(&start, 2);
+	cout << size(start) << endl;
+	print(start);
+	cout << endl;
+	cout <<pop_back(&start)<<endl;
+	cout << size(start) << endl;
+	print(start);
+	
+	return 0;
+}
+
---
+++ subset.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+
+using namespace std;
+
+struct subset_node
+{
+    int key;
+    subset_node* left;
+    subset_node* right;
+    subset_node* parent;
+    int level;
+};
+
+bool init(subset_node **sn)
+{
+    *sn = NULL;
+}
+
+bool insert(subset_node **sn, int k)
+{
+    subset_node* tmp = new subset_node;
+    tmp->key = k;
+    tmp->left = NULL;
+    tmp->right = NULL;
+    subset_node* predok = NULL;
+    while (*sn != NULL)
+    {
+        if ( (k > (*sn)->key) && ((*sn)->right == NULL) ) predok = *sn;
+        if ( (k < (*sn)->key) && ((*sn)->left == NULL) ) predok = *sn;
+        if (k > (*sn)->key) {subset_node** t = sn; sn = &((*t)->right);}
+        else if (k < (*sn)->key) {subset_node** t1 = sn; sn = &((*t1)->left);}
+        else if (k == (*sn)->key) {delete tmp; return false;};
+    }
+    *sn = tmp;
+    (*sn)->parent = predok;
+    if((*sn)->parent) (*sn)->level = (*sn)->parent->level + 1;
+    else (*sn)->level = 1;
+    return true;
+}
+
+unsigned int size_of_tree(subset_node *sn)
+{
+    int zero = 0;
+    unsigned int sb = 0, sm = 0;
+    if (sn == NULL) return zero;
+    if (sn != NULL)
+    {
+        sb = size_of_tree(sn->left);
+        sm = 1 + size_of_tree(sn->right);
+    }
+    return (sm+sb);
+}
+
+
+void set_level (subset_node **sn)
+{
+    if (!(*sn)) return;
+    if((*sn)->parent) (*sn)->level = (*sn)->parent->level + 1;
+    else (*sn)->level = 1;
+    set_level(&(*sn)->left);
+    set_level(&(*sn)->right);
+}
+
+unsigned int vshir (subset_node *sn, int* keys, unsigned int nomer)///////////////////////////////// в ширину начало
+{
+    unsigned int num = nomer;
+    if (sn != NULL)
+    {
+        nomer = vshir((sn->left),keys,nomer);
+        keys[nomer] = sn->key;
+        nomer = nomer + 1;
+        nomer = vshir((sn->right),keys,nomer);
+    }
+    else
+    {
+        return nomer;
+    }
+    return nomer;
+}
+
+int* DFS (subset_node *sn)
+{   unsigned int n = size_of_tree(sn);
+    if (n>0)
+    {
+        int* keys = new int [n];
+        vshir (sn,keys,0);
+        return keys;
+    }
+    else return NULL;
+} //////////////////////////////////////////////////////////////////////////в ширину конец
+
+subset_node* find(subset_node *sn, int k)
+{
+    if (size_of_tree(sn)==0) return NULL;
+    while (sn != NULL)
+    {
+        if (k > sn->key) sn = sn->right;
+            else if (k < sn->key) sn = sn->left;
+                else if (k == sn->key) {return sn;};
+    }
+
+    return NULL;
+}
+
+void pottyagivay_k_bate (bool right, subset_node *batya, subset_node *pott)
+{
+    if (right)
+    {
+        batya->right = pott;
+        batya->right->parent = batya;
+    }
+    else
+    {
+        batya->left = pott;
+        batya->left->parent = batya;
+    }
+}
+
+bool delete_root(subset_node **root)
+{
+    subset_node* predok;
+    subset_node** tmp;
+    if ((*root)->right)
+    {
+        tmp = &((*root)->right);
+        while (*tmp!= NULL)
+        {
+            if  ((*tmp)->left == NULL)  predok = *tmp;
+            tmp = &((*tmp)->left);
+        }
+        *tmp = (*root)->left;
+        if (*tmp) {(*tmp) ->parent = predok;};
+        subset_node* newroot = (*root)->right;
+        newroot->parent = NULL;
+        delete *(root);
+        *root = newroot;
+
+        return true;
+    }
+    else if ((*root)->left)
+    {
+        subset_node* newroot = (*root)->left;
+        delete (*root);
+        newroot->parent = NULL;
+        *root = newroot;
+
+        return true;
+    }
+    else
+    {
+        delete (*root);
+        *root = NULL;
+
+        return true;
+    }
+}
+
+bool remove(subset_node **sn, int k)
+{
+    if (size_of_tree(*sn)==0) return false;
+    subset_node* rem_e = (find (*sn, k));
+    if (!rem_e) return false;
+    if (k == (*sn)->key) {delete_root(sn); set_level(sn); return true;};
+    bool right = (rem_e->parent->right == rem_e);
+    subset_node** tmp;
+    subset_node* predok;
+    if (!rem_e) return false;
+    if (rem_e ->right)
+    {
+        tmp = &(rem_e ->right);
+        pottyagivay_k_bate(right,rem_e->parent,*tmp);
+        while (*tmp!= NULL)
+        {
+            if  ((*tmp)->left == NULL)  predok = *tmp;
+            tmp = &((*tmp)->left);//////////////////////////// tmp - указатель на элемент, куда нужно вставить rem_e->left
+        }
+        *tmp = rem_e->left;
+        if (*tmp) { (*tmp)->parent = predok;};
+        delete rem_e;
+
+        set_level(sn);
+        return true;
+    }
+    else if (rem_e->left)
+    {
+        pottyagivay_k_bate(right,rem_e->parent,rem_e->left);
+        delete rem_e;
+
+        set_level(sn);
+        return true;
+    }
+    else
+    {
+        if (right) rem_e->parent->right = NULL;
+        else rem_e->parent->left = NULL;
+        delete rem_e;
+
+        set_level(sn);
+        return true;
+    }
+
+}
+
+void h(subset_node *sn, unsigned int* i)
+{
+    if (!sn) return;
+    if (sn -> level > *i) *i = sn->level;
+    h(sn->left, i);
+    h(sn->right, i);
+    return;
+}
+
+unsigned int height(subset_node *sn)
+{
+    unsigned int* i = new unsigned int;
+    *i = 0;
+    h(sn,i);
+    unsigned int hei = *i;
+    delete i;
+    return (hei);
+}
+
+void destructor(subset_node **sn)
+{
+    if (!*sn) return;
+    destructor(&((*sn)->left));
+    destructor(&((*sn)->right));
+    delete *sn;
+    *sn = NULL;
+    return;
+}
